8d3d31bd51b2d44c1458e2081ae4d8b3c90afb61
virendra038 <virendra@fabfresh.in>
2016-04-20 18:58:37 +0530

    Update settings.py

--- django_social_project/settings.py
+++ django_social_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_social_project',
+    'django_social_app',
     'social.apps.django_app.default',
 )
 
